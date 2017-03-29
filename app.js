6de44cef75da1b615bdb1a59d4f7605c8d96ea6b
Sebas Sebas <seplarui@gmail.com>
2017-03-29 22:46:39 +0200

    CRUD

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
 var express = require("express"),
-    app = express(),
+    app = expres(),
     bodyParser = require("body-parser"),
-    methodOverride = require("method-override");
+    methodOverride = require("method-override"),
     mongoose = require('mongoose');
+
+
+//Connection to BD
+
+mongoose.connect('mongodb://localhost/tvshows', function (err, res) {
+    if (err) throw err;
+    console.log('Connected to Database');
+});
+
+//Middlewares
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+//Import Models and Controllers
+
+var models = require('./models/tvshow')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
+
+//Example Route
 
 var router = express.Router();
-
 router.get('/', function (req, res) {
-    res.send("Hola Mundo");
+    res.send("Hello World");
 });
 
 app.use(router);
-//var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tvshows', function (err, res) {
-    if (err) {
-        console.log('Error: connecting to Database. ' + err);
-    }
 
-    app.listen(3000, function () {
-        console.log("Node server running on http://localhost:3000");
-    });
+//API Routes
+
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+    .get(TVShowCtrl.findAllTVShows)
+    .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+    .get(TVShowCtrl.findById)
+    .put(TVShowCtrl.updateTVShow)
+    .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+//Start Server
+
+app.listen(3000, function () {
+    console.log("Node server running on http://localhost:3000");
 });
-
-
-
-

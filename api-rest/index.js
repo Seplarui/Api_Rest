2f56865f3ea3b9d9c5df56ee23a28020af5b01c0
Sebas Sebas <seplarui@gmail.com>
2017-04-02 23:07:39 +0200

    Listar productos

--- api-rest/index.js
+++ api-rest/index.js
@@ -13,11 +13,24 @@ app.use(bodyParser.json());
 
 app.get('/api/product', (req, res) => {
 
-    res.send(200, { products: [] })
+    Product.find({}, (err, products) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la petición: ${err}` })
+        if (!products) return res.status(404).send({ message: 'No existen productos' })
 
+        res.send(200, { products })
+
+    })
 })
+//BUSCA UN OBJETO EN LA BDA CON UN ID CONCRETO
+app.get('/api/product/:productId', (req, res) => {
 
-app.get('api/product/:productId', (req, res) => {
+    let productId = req.params.productId
+
+    Product.findById(productId, (err, product) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la petición: ${err}` })
+        if (!product) return res.status(404).send({ message: `El producto no existe` })
+        res.status(200).send({ product })
+    })
     
 })
 

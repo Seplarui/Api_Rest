59012db48dbaa1e300302330d0f56a79c95c626b
Sebas Sebas <seplarui@gmail.com>
2017-04-02 22:51:37 +0200

    Insertar en bbdd Mongo

--- api-rest/index.js
+++ api-rest/index.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Product = require('./models/product')
 
 const app = express()
 const port = process.env.port|| 3001
@@ -21,10 +23,25 @@ app.get('api/product/:productId', (req, res) => {
 
 app.post('/api/product', (req, res) => {
 
+    console.log('POST /api/product')
     console.log(req.body)
-    res.status(404).send({message: 'El producto no existe'})
 
-})
+    //ALMACENAR EN LA BASE DE DATOS NUESTRO PRIMER PRODUCTO
+
+    let product = new Product()
+    product.name = req.body.name
+    product.picture = req.body.picture
+    product.price = req.body.price
+    product.category = req.category
+    product.description=req.body.description
+
+    product.save((err, productStored) => {
+        if (err) res.status(500).send({ message: `Error al salvar en la base de datos: ${err}` })
+            res.status(200).send({product: productStored})
+
+        })
+    })
+
 
 //ACTUALIZACIÓN PRODUCTO
 
@@ -36,6 +53,15 @@ app.delete('/api/product/:productId', (req, res) => {
 
 })
 
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+    if (err)  {
+        return console.log(`Error al conectar a la base de datos: ${err}`)
+    }
+    console.log('Conexión a la base de datos establecida...')
+
 app.listen(port, function() {
     console.log(`Api Rest Corriendo en http://localhost:${port}`)
-});
+    })
+
+})
+
